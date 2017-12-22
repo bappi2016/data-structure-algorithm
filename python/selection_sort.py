1c71c5dbbde1584f11ec908fa0b56cdfaa10f196
Mahmud Ahsan <mahmud@thinkdiff.net>
2017-12-22 17:37:28 +0800

    selection sort added

---
+++ python/selection_sort.py
@@ -0,0 +1,25 @@
+# Selection Sort 
+# O(n^2)
+# Take integer and sort
+
+def findSmallest(arr):
+    smallest_item  = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest_item:
+            smallest_item  = arr[i]
+            smallest_index = i
+    return smallest_index
+    
+def selectionSort(arr):
+    new_arr = []
+    for i in range(len(arr)):
+        smallest_item = arr.pop(findSmallest(arr))
+        new_arr.append(smallest_item)
+    return new_arr
+    
+#Testing
+    
+print(selectionSort([1, 2, 3, 4, 5]))
+print(selectionSort([5, 4, 3, 2, 1]))
+print(selectionSort([5, 50, 3, 100, 1]))
---
+++ swift/selection_sort.swift
@@ -0,0 +1,35 @@
+// Selection Sort 
+// O(n^2)
+// Take integer and sort
+
+
+func findSmallest(arr: [Int]) -> Int {
+    var smallestItem  = arr[0]
+    var smallestIndex = 0
+    for i in 1..<arr.count{
+        if arr[i] < smallestItem {
+            smallestItem  = arr[i]
+            smallestIndex = i
+        }
+    }
+    return smallestIndex
+}
+
+func selectionSort(_ arrOriginal: [Int]) -> [Int] {
+    var arr    = arrOriginal
+    var newArr = [Int]()
+    for _ in 0..<arr.count {
+        let smallestItemIndex = findSmallest(arr: arr)
+        let smallestItem      = arr[smallestItemIndex]
+        newArr += [smallestItem]
+        arr.remove(at: smallestItemIndex)
+    }
+    return newArr
+}
+
+
+// Testing
+
+print(selectionSort([1, 2, 3, 4, 5]))
+print(selectionSort([5, 4, 3, 2, 1]))
+print(selectionSort([5, 50, 3, 100, 1]))
