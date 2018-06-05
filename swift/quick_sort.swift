9766a90ba4c2a5b98310e4fc5668f9d27f44c9d8
Mahmud Ahsan <mahmud@thinkdiff.net>
2018-06-06 00:49:22 +0800

    assert added quick_sort

--- swift/quick_sort.swift
+++ swift/quick_sort.swift
@@ -17,7 +17,6 @@ func quickSort <T: Comparable> (_ array: [T]) -> [T] {
 }
 
 // Testing
-
-print(quickSort([1, 2, 4, 4, 5, 1, 3]))
-print(quickSort([5.0, 4.0, 3.0, 2.0, 1.0]))
-print(quickSort(["love", "like", "play", "cool", "my"]))
+assert([1, 1, 2, 3, 4, 4, 5] == quickSort([1, 2, 4, 4, 5, 1, 3]))
+assert([1.0, 2.0, 3.0, 4.0, 5.0] == quickSort([5.0, 4.0, 3.0, 2.0, 1.0]))
+assert(["cool", "like", "love", "my", "play"] == quickSort(["love", "like", "play", "cool", "my"]))
